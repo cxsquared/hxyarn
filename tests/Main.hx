1ed9e18df155d12d1ec9c6562f9de4cba9dcfc8d
Cody Claborn <cxsquared@gmail.com>
2022-02-06 17:45:21 -0500

    Cleaned up test suite

--- tests/Main.hx
+++ tests/Main.hx
@@ -11,36 +11,50 @@ class Main {
 	}
 
 	static function runYarn() {
-		// runTest('./yarns/Basic.yarn', './yarns/testcases/Basic.testplan');
-		// runTest('./yarns/Options.yarn', './yarns/testcases/Options.testplan');
-		// runTest('./yarns/Expressions.yarn', './yarns/testcases/Expressions.testplan');
-		runTest('./yarns/Commands.yarn', './yarns/testcases/Commands.testplan');
-		// runTest('./yarns/FormatFunctions.yarn', './yarns/testcases/FormatFunctions.testplan');
-		// runFunctionTest('./yarns/Functions.yarn', './yarns/testcases/Functions.testplan');
-		// runTest('./yarns/IfStatements.yarn', './yarns/testcases/IfStatements.testplan');
-		// runTest('./yarns/InlineExpressions.yarn', './yarns/testcases/InlineExpressions.testplan');
-		// runTest('./yarns/Lines.yarn', './yarns/testcases/Lines.testplan');
-		// runTest('./yarns/NodeHeaders.yarn', './yarns/testcases/NodeHeaders.testplan');
-		// runTest('./yarns/ShortcutOptions.yarn', './yarns/testcases/ShortcutOptions.testplan');
-		// runTest('./yarns/Smileys.yarn', './yarns/testcases/Smileys.testplan');
-		// runTest('./yarns/Types.yarn', './yarns/testcases/Types.testplan');
-		// runTest('./yarns/VariableStorage.yarn', './yarns/testcases/VariableStorage.testplan');
+		// Passing
+		// runTest('./yarns/yarn/Basic.yarn', './yarns/testcases/Basic.testplan');
+		// Passing
+		// runTest('./yarns/yarn/Commands.yarn', './yarns/testcases/Commands.testplan');
+		// Passing
+		// runTest('./yarns/yarn/Expressions.yarn', './yarns/testcases/Expressions.testplan');
+		// Passing
+		// runTest('./yarns/yarn/Options.yarn', './yarns/testcases/Options.testplan');
+		// Failing
+		// runTest('./yarns/yarn/FormatFunctions.yarn', './yarns/testcases/FormatFunctions.testplan');
+		// Passing
+		// runFunctionTest('./yarns/yarn/Functions.yarn', './yarns/testcases/Functions.testplan');
+		// Failing
+		// runTest('./yarns/yarn/IfStatements.yarn', './yarns/testcases/IfStatements.testplan');
+		// Failing
+		// runTest('./yarns/yarn/InlineExpressions.yarn', './yarns/testcases/InlineExpressions.testplan');
+		// Failing
+		// runTest('./yarns/yarn/Lines.yarn', './yarns/testcases/Lines.testplan');
+		// Passing
+		// runTest('./yarns/yarn/NodeHeaders.yarn', './yarns/testcases/NodeHeaders.testplan');
+		// Failing
+		// runTest('./yarns/yarn/ShortcutOptions.yarn', './yarns/testcases/ShortcutOptions.testplan');
+		// Failing
+		// runTest('./yarns/yarn/Smileys.yarn', './yarns/testcases/Smileys.testplan');
+		// Failing
+		// runTest('./yarns/yarn/Types.yarn', './yarns/testcases/Types.testplan');
+		// Failing
+		// runTest('./yarns/yarn/VariableStorage.yarn', './yarns/testcases/VariableStorage.testplan');
 	}
 
 	static function runJson() {
-		runTest('./yarns/Basic.json', './yarns/testcases/Basic.testplan');
-		runTest('./yarns/Commands.json', './yarns/testcases/Commands.testplan');
-		runTest('./yarns/Expressions.json', './yarns/testcases/Expressions.testplan');
-		runTest('./yarns/FormatFunctions.json', './yarns/testcases/FormatFunctions.testplan');
-		runFunctionTest('./yarns/Functions.json', './yarns/testcases/Functions.testplan');
-		runTest('./yarns/IfStatements.json', './yarns/testcases/IfStatements.testplan');
-		runTest('./yarns/InlineExpressions.json', './yarns/testcases/InlineExpressions.testplan');
-		runTest('./yarns/Lines.json', './yarns/testcases/Lines.testplan');
-		runTest('./yarns/NodeHeaders.json', './yarns/testcases/NodeHeaders.testplan');
-		runTest('./yarns/ShortcutOptions.json', './yarns/testcases/ShortcutOptions.testplan');
-		runTest('./yarns/Smileys.json', './yarns/testcases/Smileys.testplan');
-		runTest('./yarns/Types.json', './yarns/testcases/Types.testplan');
-		runTest('./yarns/VariableStorage.json', './yarns/testcases/VariableStorage.testplan');
+		runTest('./yarns/json/Basic.json', './yarns/testcases/Basic.testplan');
+		runTest('./yarns/json/Commands.json', './yarns/testcases/Commands.testplan');
+		runTest('./yarns/json/Expressions.json', './yarns/testcases/Expressions.testplan');
+		runTest('./yarns/json/FormatFunctions.json', './yarns/testcases/FormatFunctions.testplan');
+		runFunctionTest('./yarns/json/Functions.json', './yarns/testcases/Functions.testplan');
+		runTest('./yarns/json/IfStatements.json', './yarns/testcases/IfStatements.testplan');
+		runTest('./yarns/json/InlineExpressions.json', './yarns/testcases/InlineExpressions.testplan');
+		runTest('./yarns/json/Lines.json', './yarns/testcases/Lines.testplan');
+		runTest('./yarns/json/NodeHeaders.json', './yarns/testcases/NodeHeaders.testplan');
+		runTest('./yarns/json/ShortcutOptions.json', './yarns/testcases/ShortcutOptions.testplan');
+		runTest('./yarns/json/Smileys.json', './yarns/testcases/Smileys.testplan');
+		runTest('./yarns/json/Types.json', './yarns/testcases/Types.testplan');
+		runTest('./yarns/json/VariableStorage.json', './yarns/testcases/VariableStorage.testplan');
 	}
 
 	static function runTest(file:String, testPlan:String) {
